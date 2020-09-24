47619cc0970e5d7e4db10321c13a1b4c5c1bb025
WooYoung Cho <wy1.cho@samsung.com>
2020-09-24 10:42:12 +0900

    Update cwe.js

--- cwe.js
+++ cwe.js
@@ -48,10 +48,3 @@
         var sum = add(1, 2, 3);
         console.log(sum);
     }
-
-    function CWE_670(x) { // STRAY_SEMICOLON
-        while (++x <= 10) ;
-        {
-            sum += x;
-        }
-    }
